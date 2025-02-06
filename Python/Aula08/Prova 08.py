5d73e116f02bbf2f714a587aa3d110a22027a80d
Samuel Alves <153463165+samuelalvesv@users.noreply.github.com>
2025-02-05 21:28:36 -0300

    Add files via upload

---
+++ Python/Aula08/Prova 08.py
@@ -0,0 +1,9 @@
+# [PYIA-A08] Importe o módulo 'os' e use a função 'listdir' para listar todos os arquivos e pastas presentes no diretório onde o script Python está sendo executado. 
+# A função 'listdir' retornará uma lista contendo os nomes dos arquivos e pastas. Após obter essa lista, exiba cada item da lista individualmente na saída do console.
+
+from os import listdir
+
+itens = listdir()
+
+for item in itens:
+    print(item)
